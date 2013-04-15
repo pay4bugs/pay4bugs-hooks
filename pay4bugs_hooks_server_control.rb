e5b1309a148b398aef358b12307d78041f3722a8
Larry Salibra <larry@appartisan.com>
2013-04-15 23:58:37 +0800

    daemonize server

---
+++ pay4bugs_hooks_server_control.rb
@@ -0,0 +1,8 @@
+require 'daemons'
+options = {}
+if ARGV[1] && File.directory?(ARGV[1])
+  options = {dir_mode: :normal, dir: ARGV[1]}
+else
+  p "No valid PID directory given"
+end
+Daemons.run('pay4bugs_hooks_server.rb' )
